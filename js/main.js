af0f8724ea52ff8292fd6d655f792bb7a286d617
Shamim <amshamim99@gmail.com>
2024-01-29 15:24:01 +0600

    header and banner section done

--- js/main.js
+++ js/main.js
@@ -11,6 +11,12 @@ $('.video-popup').magnificPopup({
   type: 'iframe'
   
 });
-
+var typed= new Typed(".text",{
+  strings:['Frontend Developer','Youtuber','Web Developer'],
+  typeSpeed:100,
+  backSpeed:100,
+  backDelay:1000,
+  loop:true
+});
 
 })(jQuery);
